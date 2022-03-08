65fa4f0f2f82db948e20117bf809909994306991
amayakvn <77815761+amayakvn@users.noreply.github.com>
2022-03-08 00:18:35 -0600

    Amaya

---
+++ APLICACION/App.js
@@ -0,0 +1,13 @@
+import Login from './src/componentes/login';
+import Registro from './src/componentes/pantallaRegistro';
+import Perfil from './src/componentes/pantallaPerfil';
+import Correo from './src/componentes/pantallaCorreo';
+
+export default function App() {
+  return (
+    <Registro></Registro>
+    //<Login></Login>
+    //<Perfil></Perfil>
+    //<Correo></Correo>
+  );
+}

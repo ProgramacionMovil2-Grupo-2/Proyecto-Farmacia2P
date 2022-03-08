033521b3dc72ad79312ddbbb8b885bc00fe26d83
Sandra Calderon <sandra21jackelin@gmail.com>
2022-03-08 10:50:20 -0600

    app

--- APLICACION/app.js
+++ APLICACION/app.js
@@ -1,1 +1,15 @@
-//APLICACION
+import TipoEntrega from './src/componentes/tipoEntrega';
+import Sucursal from './src/componentes/sucursal';
+import ConfirmarSucursal from './src/componentes/confirmarSucursal';
+import Domicilio from './src/componentes/domicilio';
+import ConfirmarDomicilio from './src/componentes/confirmarDomicilio';
+export default function App() {
+  return (
+    //  <TipoEntrega></TipoEntrega>,
+    // <TipoEntrega></TipoEntrega>
+    //<Sucursal></Sucursal>
+    // <ConfirmarSucursal></ConfirmarSucursal>
+   // <Domicilio></Domicilio>
+   <ConfirmarDomicilio></ConfirmarDomicilio>
+  );
+}
